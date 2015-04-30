a7a551cfb66126a6ca60f1ce6c0f8b36e63139b5
peter <a184999@gmail.com>
2015-04-30 22:44:32 +0800

    [update]Apk_prodect

--- Apk_protect_detecter.py
+++ Apk_protect_detecter.py
@@ -15,23 +15,23 @@ Bangcle = ["libsecexe.x86.so",
 	    "libsecmain.x86.so",
 	     "libsecexe.so",
 	     "libsecmain.so"]
-QQ = []
+QQ = ["libshell.so"]
 Baidu = ["libbaiduprotect.so"]
-protect_360 = []
+protect_360 = ["libprotectClass.so"]
 Ijiami = ["libexecmain.so",
 	"ijiami.dat",
 	"libexecmain.so "]
 APKProtect = ["libspkprotect2.so",
 		"libAPKProtect.so"]
 NAGA = ["libddog.so"]
-PayEgis = []
-NQ = []
-Ali = []
+PayEgis = ["libNSaferOnly.so"]
+NQ = ["libnqshield.so"]
+Ali = ["libmobisec.so"]
 class Apk_protect_detecter:
 	def __init__(self, apkPath,apkUnzipPath):
 		self.apkPath = apkPath
 		self.apkUnzipPath =apkUnzipPath
-	def Unzip():
-		pass
-	def check():
+	def Unzip(self):
+		os.system("unzip "+self.apkPath+"-d "+self.apkUnzipPath)
+	def check(self):
 		pass
