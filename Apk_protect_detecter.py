1b378903ba3b8d5362fccaec4a752289cbc62956
peter <a184999@gmail.com>
2015-04-30 17:42:33 +0800

    [new]Apk_protect

---
+++ Apk_protect_detecter.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+""""""
+support_list = ["Bangcle",
+		"QQ",
+		"Baidu",
+		"protect_360",
+		"Ijiami",
+		"APKProtect",
+		"NAGA",
+		"PayEgis",
+		"NQ",
+		"Ali"]
+Bangcle = ["libsecexe.x86.so"]
+QQ = []
+Baidu = []
+protect_360 = []
+Ijiami = []
+APKProtect = []
+NAGA = []
+PayEgis = []
+NQ = []
+Ali = []
+class Apk_protect_detecter:
+	def __init__(self, apkPath,apkUnzipPath):
+		self.apkPath = apkPath
+		self.apkUnzipPath =apkUnzipPath
+	def Unzip():
+		pass
+	def check():
+		pass
