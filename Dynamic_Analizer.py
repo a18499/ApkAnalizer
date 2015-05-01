972c8da20ea92140f719ffd4972ced1eaca67529
peter <a184999@gmail.com>
2015-05-01 15:55:04 +0800

    [new]Dynamic_Analizer.py

---
+++ Dynamic_Analizer.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+class Dynamic_Analizer():
+	"""docstring for Dynamic_Analizer"""
+	def __init__(self, apk_filepath):
+		self.apk_filepath = apk_filepath
+		
+	def repackage(self):
+		os.system("python APIMonitor-beta/apimonitor.py "+self.apk_filepath)
+	def app_OB(self):
+		APKdeob_path = "/home/peter/APKdeOB-v4/APKdeob.jar"
+		os.system("java -jar "+APKdeob_path+"  -i "+self.apk_filepath+" -d Nexus_5_API_19 -w /home/peter/APKdeOB_tmp/")
+	 
+if __name__ == '__main__':
+	obj = Dynamic_Analizer(sys.argv[1])
+	obj.app_OB()
